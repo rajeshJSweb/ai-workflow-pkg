0c5506f8175af136a74e91a2ee6797a2dff4dd6d
Rajesh Khoksi <rjshkhoksi26@gmail.com>
2025-09-06 15:47:44 +0600

    Update the package

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ const handleAIFunctionWorkflow = async (
     getChatSession,
     createTransaction,
     checkOrderDetails,
-    processMessage,
     fetchKnowledgeBasedData,
     assignHumanAgent,
     createOrder,
+    getFAQAnswer,
+    fetchCombinedProductData,
   } = handlers;
 
   // if (providedSecret !== "a11cf9f7-bda2-48a0-be3a-c56fef2b053a25") {
@@ -31,9 +32,18 @@ const handleAIFunctionWorkflow = async (
   const instructions = await Instruction.find().lean();
   const data = app.knowledgeBase.instructions.find((curElm) => curElm.isActive);
 
-  const systemInstruction = instructions.find((curElm) => curElm.tool === app?.service);
+  const systemInstruction = instructions.find(
+    (curElm) => curElm.tool === app?.service
+  );
 
-  const chat = getChatSession(senderId, data?.instruction, systemInstruction?.instruction || systemPrompt);
+  const defaultInstruction =
+    "You are a helpful, knowledgeable, and friendly assistant for our brand who communicates clearly and concisely. Always be polite and professional.";
+
+  const chat = getChatSession(
+    senderId,
+    data?.instruction,
+    systemInstruction?.instruction || defaultInstruction
+  );
   const roleMap = { user: "User", function: "Data", assistant: "Assistant" };
 
   if (chat?.historyInternal) {
@@ -54,53 +64,88 @@ const handleAIFunctionWorkflow = async (
     const candidates = response.response?.candidates || response.candidates;
     const functionCall = candidates?.[0]?.content?.parts?.[0]?.functionCall;
 
+    console.log("Function Call:", functionCall);
+
     if (functionCall) {
       switch (functionCall.name) {
         case "fetchProductData": {
           const searchParams = functionCall.args;
-          const { results, explanation, images } = await processMessage(
+          const googleDataFromMongo = await fetchCombinedProductData(
             app_id,
-            historyContext,
-            sessionId,
-            from,
-            senderId,
             searchParams
           );
+          console.log("From Workflow:-", googleDataFromMongo.data.length);
 
           const result = await chat.sendMessage([
             {
               functionResponse: {
                 name: "fetchProductData",
-                response: { results, explanation },
+                response: {
+                  results: googleDataFromMongo.data,
+                  explanation: null,
+                },
               },
             },
           ]);
-
           return {
             responseContent:
               result.response.candidates[0]?.content?.parts[0]?.text,
-            images,
+            searchParams: searchParams,
           };
         }
 
-        case "getCompanyInfo":
+        case "getKnowledgebaseAnswer": {
+          const { response } = await fetchKnowledgeBasedData(
+            messageText,
+            app_id
+          );
+          const result = await chat.sendMessage([
+            {
+              functionResponse: {
+                name: "getKnowledgebaseAnswer",
+                response: {
+                  results: response,
+                },
+              },
+            },
+          ]);
           return {
-            responseContent: await fetchKnowledgeBasedData(messageText, app_id),
+            responseContent:
+              result.response.candidates[0]?.content?.parts[0]?.text,
           };
+        }
+
+        case "getFAQAnswer": {
+          const { response } = await getFAQAnswer(messageText, app_id);
+
+          const result = await chat.sendMessage([
+            {
+              functionResponse: {
+                name: "getFAQAnswer",
+                response: {
+                  results: response,
+                },
+              },
+            },
+          ]);
+          return {
+            responseContent:
+              result.response.candidates[0]?.content?.parts[0]?.text,
+          };
+        }
 
         case "submitOrder": {
           const orderDetails = functionCall.args.order_details;
           const newOrder = await createOrder(orderDetails, app_id, senderId);
+
+          // console.log("Order details args", orderDetails);
           return {
-            responseContent: `✅ Your order has been successfully submitted!\n\n🛒 *Order Confirmation Number:* **${newOrder.orderNumber}**`,
+            responseContent: `✅ Your order has been successfully submitted!\n\n🛒 *Order Confirmation Number:* **${newOrder.orderNumber}**\n\nThank you for shopping with us! 🎉`,
           };
         }
 
         case "collectPaymentInfo": {
           const transactionData = functionCall.args;
-
-          if (!createTransaction)
-            throw new Error("createTransaction handler not provided");
           const { responseContent } = await createTransaction(transactionData);
           return { responseContent };
         }
@@ -113,13 +158,11 @@ const handleAIFunctionWorkflow = async (
             senderId,
             reason
           );
+
           return { responseContent };
         }
 
         case "checkOrderDetails": {
-          if (!checkOrderDetails)
-            throw new Error("checkOrderDetails handler not provided");
-
           const { orderNumber } = functionCall.args;
           const { responseContent, otp, order } = await checkOrderDetails(
             orderNumber,
@@ -130,11 +173,36 @@ const handleAIFunctionWorkflow = async (
             {
               functionResponse: {
                 name: "checkOrderDetails",
-                response: { explanation: responseContent, otp, order },
+                response: {
+                  explanation: responseContent,
+                  otp: otp,
+                  order,
+                },
               },
             },
           ]);
 
+          return {
+            responseContent:
+              result.response.candidates[0]?.content?.parts[0]?.text,
+          };
+        }
+        case "getCompanyInfo": {
+          const { response } = await fetchKnowledgeBasedData(
+            messageText,
+            app_id
+          );
+
+          const result = await chat.sendMessage([
+            {
+              functionResponse: {
+                name: "getCompanyInfo",
+                response: {
+                  results: response,
+                },
+              },
+            },
+          ]);
           return {
             responseContent:
               result.response.candidates[0]?.content?.parts[0]?.text,
@@ -146,7 +214,7 @@ const handleAIFunctionWorkflow = async (
     const textResponse = candidates?.[0]?.content?.parts?.[0]?.text;
     return { responseContent: textResponse };
   } catch (error) {
-    console.error("Error in handleAIFunctionWorkflow:", error);
+    console.error("Error in handleConversationFlow:", error);
     throw error;
   }
 };
